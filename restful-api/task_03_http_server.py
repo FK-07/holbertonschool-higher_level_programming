ea81c5cd79c9da051e6cfeae71e32c563ca0f0b4
FK-07 <kazimli.frid2007@gmail.com>
2026-08-07 14:59:08 +0000

    Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -44,7 +44,7 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == '__main__':
