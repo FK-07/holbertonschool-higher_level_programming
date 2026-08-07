76c2f71a74a0f9c139261972961c34c088af7c4d
FK-07 <kazimli.frid2007@gmail.com>
2026-08-07 14:02:59 +0000

    Add Task 9 script using requests for GitHub API authentication

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Uses the GitHub API to display user id with Basic Authentication"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    r = requests.get(url, auth=(username, password))
+    print(r.json().get('id'))
